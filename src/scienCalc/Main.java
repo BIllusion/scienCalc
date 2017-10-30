995d937d797638ec172ec42cddd4c29e69b53823
BIllusion <lol@lol.de>
2017-10-30 02:06:37 +0100

    test

--- src/scienCalc/Main.java
+++ src/scienCalc/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello W1orld");
+        primaryStage.setTitle("Hello W1orsld");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
