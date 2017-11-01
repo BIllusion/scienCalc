0e55aceddb903879da0ea954a98f1282bb3356a3
BIllusion <ruess.christoph@gmail.com>
2017-11-01 19:38:57 +0100

    PCTest1

--- src/scienCalc/Main.java
+++ src/scienCalc/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello W1orsld");
+        primaryStage.setTitle("Hello W1orsldPC");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
