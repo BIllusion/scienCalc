2501980437f548980ffd6266e291e7f473d1ba6a
BIllusion <ruess.christoph@gmail.com>
2017-11-06 18:57:26 +0100

    Grid-Placement & Resizing fixed

--- src/scienCalc/TRechnerGUI.java
+++ src/scienCalc/TRechnerGUI.java
@@ -3,10 +3,8 @@ package scienCalc;
 import javafx.geometry.Insets;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
+import javafx.scene.text.Font;
 
 public class TRechnerGUI {
 
@@ -17,67 +15,94 @@ public class TRechnerGUI {
     private VBox outerVBox, innerVBox;
     private HBox gridHBox;
     private GridPane funcGridPane, nrGridPane;
+    private Button btn;
 
     public TRechnerGUI() {
 
+        // Base Anchor init
         baseAnchorPane = new AnchorPane();
         baseAnchorPane.setPadding(new Insets(MAINPADDING, MAINPADDING, MAINPADDING, MAINPADDING));
         baseAnchorPane.setStyle("-fx-background-color: #303030;");
 
+        // Einteilung oben unten
         outerVBox = new VBox();
         outerVBox.setFillWidth(true);
         outerVBox.setSpacing(MAINPADDING);
         outerVBox.setStyle("-fx-background-color: #32504e;"); //nur debugging
 
+        // Oberer Teil
         innerVBox = new VBox();
         innerVBox.setStyle("-fx-background-color: #FF0000;"); //nur debugging
 
+        // Unterer Teil
         gridHBox = new HBox();
         gridHBox.setFillHeight(true);
+        gridHBox.setSpacing(MAINPADDING);
         gridHBox.setStyle("-fx-background-color: #00FF00;"); //nur debugging
 
+        // Grid Resize-Helper
+        ColumnConstraints col = new ColumnConstraints();
+        col.setHgrow(Priority.ALWAYS);
+        RowConstraints row = new RowConstraints();
+        row.setVgrow(Priority.ALWAYS);
+
+        // Funktions Grid 5 x 5
         funcGridPane = new GridPane();
         funcGridPane.setHgap(INNERPADDING);
         funcGridPane.setVgap(INNERPADDING);
         funcGridPane.setGridLinesVisible(true);
-        funcGridPane.add(new Button(), 4,4);
-        funcGridPane.setWidth(gridHBox.getWidth());
-        System.out.println("ich tu was");
-        updateGridHeight();
-        updateGridWidth();
+        funcGridPane.getColumnConstraints().addAll(col,col,col,col,col);
+        funcGridPane.getRowConstraints().addAll(row,row,row,row,row);
 
-        gridHBox.getChildren().addAll(funcGridPane);
+        // Number Grid 4 x 5
+        nrGridPane = new GridPane();
+        nrGridPane.setHgap(INNERPADDING);
+        nrGridPane.setVgap(INNERPADDING);
+        nrGridPane.setGridLinesVisible(true);
+        nrGridPane.getColumnConstraints().addAll(col,col,col,col);
+        nrGridPane.getRowConstraints().addAll(row,row,row,row,row);
 
-        outerVBox.getChildren().addAll(innerVBox, gridHBox);
+        btn = new Button();
+        btn.setMaxWidth(Double.MAX_VALUE);
+        btn.setMaxHeight(Double.MAX_VALUE);
+        btn.setText("Hallo");
+        btn.setFont(Font.font(125));
+        funcGridPane.setFillWidth(btn, true);
+        funcGridPane.setFillHeight(btn, true);
 
+
+        funcGridPane.add(btn, 2,2);
+
+
+        // GUI-Aufbau
         baseAnchorPane.setTopAnchor(outerVBox,0.0);
         baseAnchorPane.setLeftAnchor(outerVBox,0.0);
         baseAnchorPane.setRightAnchor(outerVBox,0.0);
         baseAnchorPane.setBottomAnchor(outerVBox,0.0);
         baseAnchorPane.getChildren().add(outerVBox);
 
+        outerVBox.getChildren().addAll(innerVBox, gridHBox);
+
+        gridHBox.getChildren().addAll(funcGridPane, nrGridPane);
+
+
+
         // Resize inner Elements
         baseAnchorPane.widthProperty().addListener((obs, oldVal, newVal) -> {
+            funcGridPane.setPrefWidth((5.0/9.0) * (baseAnchorPane.getWidth() - (MAINPADDING * 3) ));
+            nrGridPane.setPrefWidth(((4.0/9.0) * (baseAnchorPane.getWidth() - (MAINPADDING * 3))));
+            System.out.println("HboxWidth: " + gridHBox.getWidth() + "  BaseAnchorPane width: " + baseAnchorPane.getWidth());
 
-            System.out.println("geht");
         });
 
         baseAnchorPane.heightProperty().addListener((obs, oldVal, newVal) -> {
-            gridHBox.setPrefHeight(0.7*(baseAnchorPane.getHeight() - (MAINPADDING * 3)));
-            innerVBox.setPrefHeight(0.3*(baseAnchorPane.getHeight() - (MAINPADDING * 3)));
-            System.out.println("geht auch");
+            gridHBox.setPrefHeight(0.7 * (baseAnchorPane.getHeight() - (MAINPADDING * 3)));
+            innerVBox.setPrefHeight(0.3 * (baseAnchorPane.getHeight() - (MAINPADDING * 3)));
+            System.out.println("HboxHeight: " + gridHBox.getHeight() + "  BaseAnchorPane height: " + baseAnchorPane.getHeight());
         });
     }
 
     public Parent asParent() {
         return baseAnchorPane;
-    }
-
-    private void updateGridHeight() {
-        funcGridPane.setPrefHeight(gridHBox.getHeight());
-    }
-
-    private void updateGridWidth() {
-        funcGridPane.setPrefWidth(((5/9)*gridHBox.getWidth())-MAINPADDING);
     }
 }
