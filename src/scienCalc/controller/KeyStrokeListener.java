5e5fadd0b889fe137b01be3e3824591f6e50652d
Wyrdeija <wyrdeija@gmail.com>
2017-11-07 16:33:25 +0100

    Keystrokes unso

--- src/scienCalc/controller/KeyStrokeListener.java
+++ src/scienCalc/controller/KeyStrokeListener.java
@@ -16,13 +16,36 @@ public class KeyStrokeListener implements EventHandler<KeyEvent> {
         this.fi = fi;
     }
 
-    private final KeyCombination keyCombinationShiftS = new KeyCodeCombination(KeyCode.S, KeyCombination.CONTROL_ANY);
+    private final KeyCombination keyCombinationExpf = new KeyCodeCombination(KeyCode.X, KeyCombination.CONTROL_ANY);
+    private final KeyCombination keyCombinationSin = new KeyCodeCombination(KeyCode.S, KeyCombination.CONTROL_ANY);
+    private final KeyCombination keyCombinationConv = new KeyCodeCombination(KeyCode.C, KeyCombination.CONTROL_ANY);
+    private final KeyCombination keyCombinationExtras = new KeyCodeCombination(KeyCode.E, KeyCombination.CONTROL_ANY);
+    private final KeyCombination keyCombinationDel = new KeyCodeCombination(KeyCode.D, KeyCombination.CONTROL_ANY);
+    private final KeyCombination keyCombinationNum = new KeyCodeCombination(KeyCode.Z, KeyCombination.CONTROL_ANY);
+    private final KeyCombination keyCombinationOp = new KeyCodeCombination(KeyCode.O, KeyCombination.CONTROL_ANY);
 
     @Override
     public void handle(KeyEvent event) {
-        if (keyCombinationShiftS.match(event)) {
-            System.out.println("KeyStroke Detected");
-            fi.getNrGrid().setButtonFocus(ActionCmds.ZERO);
+        if (keyCombinationExpf.match(event)) {
+            fi.getFuncGrid().setButtonFocus(ActionCmds.SQR);
+
+        } else if (keyCombinationSin.match(event)) {
+            fi.getFuncGrid().setButtonFocus(ActionCmds.SIN);
+
+        } else if (keyCombinationConv.match(event)) {
+            fi.getFuncGrid().setButtonFocus(ActionCmds.EXP);
+
+        } else if (keyCombinationExtras.match(event)) {
+            fi.getFuncGrid().setButtonFocus(ActionCmds.MOD);
+
+        } else if (keyCombinationDel.match(event)) {
+            fi.getNrGrid().setButtonFocus(ActionCmds.CLEARINPUT);
+
+        } else if (keyCombinationNum.match(event)) {
+            fi.getNrGrid().setButtonFocus(ActionCmds.ONE);
+
+        } else if (keyCombinationOp.match(event)) {
+            fi.getNrGrid().setButtonFocus(ActionCmds.DIVIDE);
 
         }
     }
